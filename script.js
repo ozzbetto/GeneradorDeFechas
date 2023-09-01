f583481e8e84c17ae9e8ac0e4ec53afc75c3a7ee
Betto Rodriguez <segovia.al@gmail.com>
2023-09-01 13:33:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 const empleados = ['Herminio', 'Carlos', 'Christian', 'Francisco'];
 
-function mezclarFechas() {
-    // Mezcla el array de empleados
-    for (let i = empleados.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [empleados[i], empleados[j]] = [empleados[j], empleados[i]];
+function mezclarAsignaciones(asignaciones) {
+    let todasLasFechas = [];
+    for (let empleado in asignaciones) {
+        todasLasFechas = todasLasFechas.concat(asignaciones[empleado]);
     }
-    generarFechas(); // Llama a generarFechas para refrescar la tabla con los nombres mezclados
+
+    // Mezclar todas las fechas
+    for (let i = todasLasFechas.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [todasLasFechas[i], todasLasFechas[j]] = [todasLasFechas[j], todasLasFechas[i]];
+    }
+
+    // Asignar las fechas mezcladas a los empleados
+    let nuevasAsignaciones = {};
+    empleados.forEach(empleado => {
+        nuevasAsignaciones[empleado] = [];
+    });
+
+    for (let i = 0; i < todasLasFechas.length; i++) {
+        let empleado = empleados[i % empleados.length];
+        nuevasAsignaciones[empleado].push(todasLasFechas[i]);
+    }
+
+    return nuevasAsignaciones;
 }
 
-function generarFechas() {
-    const tablaGuardias = document.getElementById('tablaGuardias');
-    tablaGuardias.innerHTML = ''; // Limpiar la tabla
+function mezclarFechas() {
+    const asignaciones = generarFechas(true); // Obtener las asignaciones sin refrescar la tabla
+    const nuevasAsignaciones = mezclarAsignaciones(asignaciones);
+    mostrarAsignaciones(nuevasAsignaciones); // Refrescar la tabla con las nuevas asignaciones
+}
 
+function generarFechas(sinMostrar = false) {
     const fechaActual = new Date();
     const mesActual = fechaActual.getMonth();
     const anoActual = fechaActual.getFullYear();
@@ -37,9 +57,20 @@ function generarFechas() {
         asignaciones[empleado].push(sábados[i]);
     }
 
+    // Llenar la tabla con las fechas asignadas
+    if (!sinMostrar) {
+        mostrarAsignaciones(asignaciones);
+    }
+
+    return asignaciones;
+}
+
+function mostrarAsignaciones(asignaciones) {
+    const tablaGuardias = document.getElementById('tablaGuardias');
+    tablaGuardias.innerHTML = ''; // Limpiar la tabla
+
     const colores = ['color1', 'color2', 'color3', 'color4'];
 
-    // Llenar la tabla con las fechas asignadas
     sábados.forEach((sábado, index) => {
         let colorFila = colores[index % colores.length];
         let fila = `<tr class="${colorFila}"><td>` + sábado.toLocaleDateString() + '</td>';

69eb452293560f580aa95e4f2d76b667ad20f2ed
Betto Rodriguez <segovia.al@gmail.com>
2023-09-01 17:37:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,10 @@
 const empleados = ['Herminio', 'Carlos', 'Christian', 'Francisco'];
 
-function mezclarFechas() {
-    const sábados = obtenerSabadosDelMes();
-    let empleadosMezclados = [...empleados];
-
-    // Mezclar el arreglo de empleados
-    for (let i = empleadosMezclados.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [empleadosMezclados[i], empleadosMezclados[j]] = [empleadosMezclados[j], empleadosMezclados[i]];
-    }
-
-    let nuevasAsignaciones = {};
-    empleados.forEach(empleado => {
-        nuevasAsignaciones[empleado] = [];
-    });
-
-    // Asignar sábados a empleados mezclados
-    for (let i = 0; i < sábados.length; i++) {
-        let empleado = empleadosMezclados[i % empleados.length];
-        nuevasAsignaciones[empleado].push(sábados[i]);
-    }
-
-    mostrarAsignaciones(nuevasAsignaciones, sábados);
-}
-
-function obtenerSabadosDelMes() {
-    const fechaActual = new Date();
-    const mesActual = fechaActual.getMonth();
-    const anoActual = fechaActual.getFullYear();
-
+function obtenerSabadosDelMes(mes) {
+    const anoActual = new Date().getFullYear();
     let sábados = [];
     for (let i = 1; i <= 31; i++) {
-        let fecha = new Date(anoActual, mesActual, i);
+        let fecha = new Date(anoActual, mes, i);
         if (fecha.getDay() === 6) { // 6 es sábado
             sábados.push(fecha);
         }
@@ -39,8 +12,8 @@ function obtenerSabadosDelMes() {
     return sábados;
 }
 
-function generarFechas(sinMostrar = false) {
-    const sábados = obtenerSabadosDelMes();
+function generarFechasParaMes(mes) {
+    const sábados = obtenerSabadosDelMes(mes);
     let asignaciones = {};
 
     empleados.forEach(empleado => {
@@ -52,26 +25,54 @@ function generarFechas(sinMostrar = false) {
         asignaciones[empleado].push(sábados[i]);
     }
 
-    if (!sinMostrar) {
-        mostrarAsignaciones(asignaciones, sábados);
-    }
-
-    return asignaciones;
+    mostrarAsignaciones(asignaciones, sábados, mes);
 }
 
-function mostrarAsignaciones(asignaciones, sábados) {
-    const tablaGuardias = document.getElementById('tablaGuardias');
-    tablaGuardias.innerHTML = ''; // Limpiar la tabla
+function generarFechasParaTodosLosMeses() {
+    const mesActual = new Date().getMonth();
+    const contenedor = document.getElementById('contenedorTablas');
+    contenedor.innerHTML = ''; // Limpiar el contenedor
 
+    for (let mes = mesActual; mes < 12; mes++) {
+        generarFechasParaMes(mes);
+    }
+}
+
+function descargarPDF() {
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF();
+    doc.text("Asignaciones de Guardias", 10, 10);
+    // Aquí puedes agregar el contenido que deseas en el PDF
+    doc.save("asignaciones.pdf");
+}
+
+function mostrarAsignaciones(asignaciones, sábados, mes) {
+    const contenedor = document.getElementById('contenedorTablas');
+    const mesesNombres = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
     const colores = ['color1', 'color2', 'color3', 'color4'];
+
+    let tabla = `<h3>${mesesNombres[mes]}</h3>
+                 <table class="table table-bordered">
+                     <thead>
+                         <tr>
+                             <th>Fecha</th>
+                             <th>Herminio</th>
+                             <th>Carlos</th>
+                             <th>Christian</th>
+                             <th>Francisco</th>
+                         </tr>
+                     </thead>
+                     <tbody>`;
 
     sábados.forEach((sábado, index) => {
         let colorFila = colores[index % colores.length];
-        let fila = `<tr class="${colorFila}"><td>` + sábado.toLocaleDateString() + '</td>';
+        tabla += `<tr class="${colorFila}"><td>` + sábado.toLocaleDateString() + '</td>';
         empleados.forEach(empleado => {
-            fila += '<td>' + (asignaciones[empleado].includes(sábado) ? '✓' : '') + '</td>';
+            tabla += '<td>' + (asignaciones[empleado].includes(sábado) ? '✓' : '') + '</td>';
         });
-        fila += '</tr>';
-        tablaGuardias.innerHTML += fila;
+        tabla += '</tr>';
     });
+
+    tabla += '</tbody></table>';
+    contenedor.innerHTML += tabla;
 }
